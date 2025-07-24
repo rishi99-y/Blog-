5743141c3766187fac385f54bcc9d8a58079e2e9
rishi99-y <rishikeshyeslote99@gmail.com>
2025-07-24 17:36:01 +0530

    1st

---
+++ myblog/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('create/', views.post_create, name='post_create'),
+    path('post/<slug:slug>/edit/', views.post_edit, name='post_edit'),
+    path('post/<slug:slug>/delete/', views.post_delete, name='post_delete'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+]
